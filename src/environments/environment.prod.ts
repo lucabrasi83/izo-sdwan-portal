8f8447e7271d8972104be0c7b16cce5ce754f634
Luca Brasi <sebastien.pouplin@tatacommunications.com>
2018-05-09 18:29:03 +0700

    Updated Prod Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
     production: true,
-    hmr       : false
+    hmr       : false,
+  firebase: {
+    apiKey: 'AIzaSyBxc6B9DceXwXvMon6qH5V7_WbuKk_8DVE',
+    authDomain: 'izo-portal.firebaseapp.com',
+    databaseURL: 'https://izo-portal.firebaseio.com',
+    projectId: 'izo-portal',
+    storageBucket: 'izo-portal.appspot.com',
+    messagingSenderId: '209932985020'
+  }
 };
